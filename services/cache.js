9299c735b1cbd305ea32b0bd75c37575ee5a0849
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-30 09:54:32 +0300

    Toggleable chache

--- services/cache.js
+++ services/cache.js
@@ -7,10 +7,19 @@ const client = redis.createClient(redisURL);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true;
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
   //   console.log("I'M ABOUT TO RUN A QUERY");
   //   console.log(this.getQuery());
   //   console.log(this.mongooseCollection.name);
+
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
 
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
