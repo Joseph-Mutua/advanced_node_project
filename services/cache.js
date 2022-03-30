db53068429b403797d89ddeb96614c9dd417499c
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-30 09:32:25 +0300

    Hydrating models

--- services/cache.js
+++ services/cache.js
@@ -8,22 +8,26 @@ client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = async function () {
-//   console.log("I'M ABOUT TO RUN A QUERY");
-//   console.log(this.getQuery());
-//   console.log(this.mongooseCollection.name);
+  //   console.log("I'M ABOUT TO RUN A QUERY");
+  //   console.log(this.getQuery());
+  //   console.log(this.mongooseCollection.name);
 
-  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  })
-)
-// See if we have a value for a key in Redis.
-const cacheValue = await client.get(key)
-// If YES, return that
-if(cacheValue){
-    console.log(cacheValue)
-}
-// If NO, issue the query and store the value in redis
- 
-const result = await exec.apply(this, arguments);
-console.log(result)
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+  // See if we have a value for a key in Redis.
+  const cacheValue = await client.get(key);
+  // If YES, return that
+  if (cacheValue) {
+    const doc = new this.model(JSON.parse(cacheValue));
+    return doc;
+  }
+
+  // If NO, issue the query and store the value in redis
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+  return result;
 };
