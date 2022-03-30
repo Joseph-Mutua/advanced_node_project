b250e4c88bd77a3a69e69a6703b655065aadea3c
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-30 14:57:15 +0300

    Asserting OAuth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -3,24 +3,26 @@ const puppeteer = require("puppeteer");
 let browser, page;
 
 beforeEach(async () => {
-   browser = await puppeteer.launch({
+  browser = await puppeteer.launch({
     headless: false,
   });
-   page = await browser.newPage();
+  page = await browser.newPage();
 
   await page.goto("localhost:3000");
-})
-
-afterEach( async () => {
-    await browser.close()
-})
-
-test("We can launch a browser", async () => {
-
-  const text = await page.$eval("a.brand-logo", el => el.innerHTML)
-  expect(text).toEqual("Blogster");
-
-
 });
 
+afterEach(async () => {
+  await browser.close();
+});
 
+test("Header has the Correct Text", async () => {
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  expect(text).toEqual("Blogster");
+});
+
+test("Clicking Login Starts OAuth Flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+  console.log(url);
+});
