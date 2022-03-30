2e0cec09eb31b7d94a55843241f9f2b3b269964e
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-30 20:43:00 +0300

    Assembling the session factory

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require("./factories/sessionFactory");
 
 let browser, page;
 
@@ -28,15 +29,14 @@ test("Clicking Login Starts OAuth Flow", async () => {
 });
 
 test("When signed in, shows logout button", async () => {
-  const id = "6242f2d55597deed971c17e2";
-
-  
-  await page.setCookie({ name: "session", value: sessionString });
+   
+  const { session, sig } = sessionFactory();
+  await page.setCookie({ name: "session", value: session });
   await page.setCookie({ name: "session.sig", value: sig });
   await page.goto("localhost:3000");
   await page.waitFor("a[href='/auth/logout']");
 
   const text = await page.$eval("a[href='/auth/logout']", (el) => el.innerHTML);
 
-  expect(text).toEqual("Logout")
+  expect(text).toEqual("Logout");
 });
