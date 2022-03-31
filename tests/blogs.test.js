d9a919c19b1c8b6cd833d18d3d32c206bfd3a27a
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-31 10:58:42 +0300

    Asserting form display

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -5,14 +5,18 @@ let page;
 beforeEach(async () => {
   page = await Page.build();
 
-  await page.goto("localhost:3000")
+  await page.goto("localhost:3000");
 });
-
 
 afterEach(async () => {
   await page.close();
 });
 
-test("When logged in, can see blog create form" , async() => {
-    await page.login()
-})
+test("When logged in, can see blog create form", async () => {
+  await page.login();
+  await page.click("a.btn-floating");
+
+  const label = await page.getContentsOf("form label");
+
+  expect(label).toEqual("Blog Title");
+});
